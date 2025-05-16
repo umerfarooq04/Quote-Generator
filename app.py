d6275da9636ab6cfadfd1ce77972b467fc172be4
iemumer.f <umerfarooq0414@gmail.com>
2025-05-16 11:03:53 +0500

    Adding updated app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
-from utils import get_random_quote
+from flask import Flask, render_template, session, redirect, url_for
+from utils import get_random_quote, save_to_history, load_history
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)  # Required for session
 
 @app.route('/')
 def index():
-    quote = get_random_quote()
-    return render_template("index.html", quote=quote)
+    quote, author = get_random_quote()
+    session['current_quote'] = (quote, author)
+    save_to_history(quote, author)
+    return render_template("index.html", quote=quote, author=author)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/like')
+def like():
+    if 'likes' not in session:
+        session['likes'] = []
+    quote, author = session.get('current_quote', ("", ""))
+    session['likes'].append((quote, author))
+    return redirect(url_for('index'))
 
+@app.route('/history')
+def history():
+    past_quotes = load_history()
+    return render_template("history.html", quotes=past_quotes)
